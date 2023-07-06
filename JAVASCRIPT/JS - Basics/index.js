f2644a7eabb4a1a4938178b0ff6df76ce5374da4
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-06 06:52:00 +0600

    javascript

---
+++ JAVASCRIPT/JS - Basics/index.js
@@ -0,0 +1,18 @@
+// First JS code
+//console.log("Hello World");
+
+let name='Tanzil';
+console.log(name);
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+const interestRate = 0.3;
+console.log(interestRate);
+
+let name = 'Tanzil';
+let age = 20;
+let isOK = true;
+let firstname = undefined;
+let colorT = null;
