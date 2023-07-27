f4271b170633e3091a62df54d3e981f633b82773
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-27 20:52:37 +0600

    Harry js tutorial

---
+++ JAVASCRIPT/harry.js
@@ -0,0 +1,10 @@
+//1. Ways to print javascript
+// console.log("Hello World");
+// alert("Hello World");
+// document.write("Hello World");
+
+//2. JS console API
+console.log("Hello World", 5+10);
+console.warn("This is warning");
+console.error("This is an error");
+
