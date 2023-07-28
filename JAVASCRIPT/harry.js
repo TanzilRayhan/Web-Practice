c854045f25a267d202367a032c0234bdad5a25a5
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-07-28 22:06:11 +0600

    js variables

--- JAVASCRIPT/harry.js
+++ JAVASCRIPT/harry.js
@@ -1,10 +1,12 @@
-//1. Ways to print javascript
+// 1. Ways to print javascript
 // console.log("Hello World");
 // alert("Hello World");
 // document.write("Hello World");
 
-//2. JS console API
-console.log("Hello World", 5+10);
+// 2. JS console API
+console.log("Hello World", 5 + 10);
 console.warn("This is warning");
 console.error("This is an error");
+
+// 3. Javascript Variables
 
